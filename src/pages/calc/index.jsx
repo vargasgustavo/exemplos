94cf6b2e6a46002ad9954a9c99ea772c8353efd3
vargasgustavo <gustavo.neumann.v@gmail.com>
2022-12-07 12:35:14 -0300

    Criação do exemplo 'checkbox'

--- src/pages/calc/index.jsx
+++ src/pages/calc/index.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 
-export default function Calc() {
-  let botao = document.getElementsByClassName("mybutton")[0];
-  botao.onclick = function (e) {
-    let num1 = Number(document.getElementById("num1").value);
-    let num2 = Number(document.getElementById("num2").value);
-    let oper = document.getElementById("oper").value;
-    if (oper === "+") {
-      alert(num1 + num2);
-    } else if (oper === "-") {
-      alert(num1 - num2);
-    } else if (oper === "/") {
-      alert(num1 / num2);
-    } else if (oper === "*") {
-      alert(num1 * num2);
-    } else {
-      alert("operação inválida");
-    }
-    e.preventDefault();
-  };
-
+function Calcular() {
+  let num1 = Number(document.getElementById("num1"));
+  let num2 = Number(document.getElementById("num2"));
+  let oper = document.getElementById("oper");
+  if (oper === "+") {
+    alert(num1 + num2);
+  } else if (oper === "-") {
+    alert(num1 - num2);
+  } else if (oper === "/") {
+    alert(num1 / num2);
+  } else if (oper === "*") {
+    alert(num1 * num2);
+  } else {
+  }
+  console.log(num1 + num2);
   return (
     <div className="App">
       <h1>Calculadora simples - React</h1>
-        <form action="#">
-            <input type="text" placeholder="Digite o primeiro numero" id="num1" />
-            <input type="text" placeholder="Digite o segundo numero" id="num2" />
-            <input type="text" placeholder= "Digite a operação + - / * " id="oper" />
-            <button className="mybutton"> Calcular </button>
-        </form>
+      <form action="#">
+        <input type="text" placeholder="Digite o primeiro numero" id="num1" /><br/><br/>
+        <input type="text" placeholder="Digite o segundo numero" id="num2" /><br/><br/>
+        <select>
+          <option id="soma">Soma</option>
+          <option id="sub">Subtração</option>
+          <option id="multi">Multiplicação</option>
+          <option id="div">Divisão</option>
+        </select>
+        <br/><br/>
+        <button id="btn" onClick={Calcular}>
+          Calcular
+        </button>
+      </form>
     </div>
   );
 }
+
+export default Calcular;
--- src/pages/checkbox/index.jsx
+++ src/pages/checkbox/index.jsx
@@ -1,7 +1,34 @@
-export default function CheckBox() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import React from "react";
+import "./styles.css";
+
+const CheckBox = () => {
+
+    function Show() {
+        var obj = document.getElementById("check").checked;
+        if (obj = true) {
+            return document.getElementById("escondido").hidden = false;
+        } else {
+            return document.getElementById("escondido").hidden = true;
+        }
+    }
+    console.log(Show)
+
+  return (
+    <div>
+      <div className="questao">
+        <h3>Você estudou em escola pública?</h3>
+        <input onClick={Show} type="checkbox" id="check" value="checar"/>
+      </div>
+      <div className="escola">
+        <input
+          hidden
+          type="text"
+          id="escondido"
+          placeholder="Escreva o nome da escola aqui"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CheckBox;
